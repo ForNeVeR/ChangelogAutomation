bbe53758b9a500ea3f2722ab41e974136f601cc6
Friedrich von Never <friedrich@fornever.me>
2021-04-17 23:01:17 +0700

    #3: add and publish an MSBuild task project

---
+++ ChangelogAutomation.MSBuild/CollectLastChangelogSectionTask.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using ChangelogAutomation.Core;
+using JetBrains.Annotations;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace ChangelogAutomation.MSBuild
+{
+    [UsedImplicitly]
+    public class CollectLastChangelogSectionTask : Task
+    {
+        [Required, PublicAPI]
+        public string ChangelogFilePath { get; set; } = null!;
+
+        [PublicAPI]
+        public string OutputType { get; set; } = "PlainText";
+
+        [Output, PublicAPI]
+        public string? ChangelogSectionContent { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(ChangelogFilePath))
+            {
+                Log.LogWarning($"Could not found changelog file {ChangelogFilePath}");
+                return false;
+            }
+
+            using var inputStream = new FileStream(ChangelogFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var inputMdStream = new MarkdownStream(inputStream);
+
+            if (OutputType.Equals("PlainText", StringComparison.OrdinalIgnoreCase))
+                ChangelogSectionContent = GetResult(
+                    new MarkdownToPlainTextConverter().ExtractVersionSectionPlainText(inputMdStream));
+            else if (OutputType.Equals("Markdown", StringComparison.OrdinalIgnoreCase))
+                ChangelogSectionContent = GetResult(
+                    new MarkdownToMarkdownConverter().ExtractVersionSection(inputMdStream));
+            else
+            {
+                Log.LogError($"Couldn't parse OutputType = {OutputType}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static string GetResult(System.Threading.Tasks.Task<MarkdownText> task) =>
+            task.GetAwaiter().GetResult().Content;
+        private static string GetResult(System.Threading.Tasks.Task<PlainText> task) =>
+            task.GetAwaiter().GetResult().Content;
+    }
+}
--- MAINTAINERSHIP.md
+++ MAINTAINERSHIP.md
@@ -7,7 +7,7 @@ Release
 To release a new version:
 1. Update the copyright year in [the license file][license], if required.
 2. Choose the new version according to [Semantic Versioning][semver]. It should consist of three numbers (e.g. `1.0.0`).
-3. Update the `<Version>` element content in the `ChangelogAutomation/ChangelogAutomation.csproj` file.
+3. Update the `<Version>` element content in the `Directory.Build.props` file.
 4. Make sure there's a properly formed version entry in [the changelog][changelog].
 5. Merge the changes via a pull request.
 6. Push a tag named `v<VERSION>` to GitHub.
