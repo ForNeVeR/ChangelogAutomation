6e083f645a30d9d75d7f1d7a814910c7d6095dd2
Friedrich von Never <friedrich@fornever.me>
2023-02-04 19:47:04 +0100

    CI: read version properly

---
+++ scripts/Get-Version.ps1
@@ -0,0 +1,24 @@
+﻿param(
+    [string] $RefName
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+
+Write-Host "Determining version from ref `"$RefName`"…"
+if ($RefName -match '^refs/tags/v') {
+    $version = $RefName -replace '^refs/tags/v', ''
+    Write-Host "Pushed ref is a version tag, version: $version"
+} else {
+    $propsFilePath = "$PSScriptRoot/../Directory.Build.props"
+    [xml] $props = Get-Content $propsFilePath
+    foreach ($group in $props.Project.PropertyGroup) {
+        if ($group.Label -eq 'Versioning') {
+            $version = $group.Version
+            break
+        }
+    }
+    Write-Host "Pushed ref is a not version tag, get version from $($propsFilePath): $version"
+}
+
+Write-Output $version
