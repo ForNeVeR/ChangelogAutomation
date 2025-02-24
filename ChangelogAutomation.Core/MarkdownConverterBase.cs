63f07134e672164d95a864cbb4306bcba449c6e4
Friedrich von Never <friedrich@fornever.me>
2025-02-24 23:24:20 +0100

    Markdig: fix nullability issues after the library update

--- ChangelogAutomation.Core/MarkdownConverterBase.cs
+++ ChangelogAutomation.Core/MarkdownConverterBase.cs
@@ -6,61 +6,60 @@ using Markdig;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 
-namespace ChangelogAutomation.Core
+namespace ChangelogAutomation.Core;
+
+public class MarkdownConverterBase
 {
-    public class MarkdownConverterBase
+    protected async Task<MarkdownDocument> ExtractVersionDocument(MarkdownStream changelogMarkdown)
     {
-        protected async Task<MarkdownDocument> ExtractVersionDocument(MarkdownStream changelogMarkdown)
-        {
-            using var reader = new StreamReader(changelogMarkdown.Stream, leaveOpen: true);
-            var text = await reader.ReadToEndAsync();
-            var document = Markdown.Parse(text);
-            var nodes = ExtractDocumentBlocksDestructively(document).ToList();
+        using var reader = new StreamReader(changelogMarkdown.Stream, leaveOpen: true);
+        var text = await reader.ReadToEndAsync();
+        var document = Markdown.Parse(text);
+        var nodes = ExtractDocumentBlocksDestructively(document).ToList();
 
-            var sectionDocument = new MarkdownDocument();
-            foreach (var block in nodes)
-                sectionDocument.Add(block);
+        var sectionDocument = new MarkdownDocument();
+        foreach (var block in nodes)
+            sectionDocument.Add(block);
 
-            return sectionDocument;
-        }
-
-        private static IEnumerable<Block> ExtractDocumentBlocksDestructively(MarkdownDocument document)
-        {
-            var referenceDefinitions = new HashSet<LinkReferenceDefinition>();
-
-            var sectionBlocks = document
-                .SkipWhile(n => n is not HeadingBlock {Level: 2})
-                .Skip(1)
-                .TakeWhile(n => n is not HeadingBlock {Level: 2});
-
-            foreach (var block in sectionBlocks)
-            {
-                if (block is LinkReferenceDefinitionGroup) continue;
-
-                foreach (var reference in ExtractReferencedLinks(block))
-                    referenceDefinitions.Add(reference);
-
-                yield return block;
-            }
-
-            document.Clear();
-
-            if (referenceDefinitions.Count <= 0) yield break;
-
-            var referenceBlock = new LinkReferenceDefinitionGroup();
-            foreach (var def in referenceDefinitions.OrderBy(d => d.Label))
-            {
-                def.Parent?.Remove(def);
-                referenceBlock.Add(def);
-            }
-
-            yield return referenceBlock;
-        }
-
-        private static IEnumerable<LinkReferenceDefinition> ExtractReferencedLinks(Block block) =>
-            block
-                .Descendants<LinkInline>()
-                .Select(li => li.Reference)
-                .Where(def => def is not null);
+        return sectionDocument;
     }
+
+    private static IEnumerable<Block> ExtractDocumentBlocksDestructively(MarkdownDocument document)
+    {
+        var referenceDefinitions = new HashSet<LinkReferenceDefinition>();
+
+        var sectionBlocks = document
+            .SkipWhile(n => n is not HeadingBlock {Level: 2})
+            .Skip(1)
+            .TakeWhile(n => n is not HeadingBlock {Level: 2});
+
+        foreach (var block in sectionBlocks)
+        {
+            if (block is LinkReferenceDefinitionGroup) continue;
+
+            foreach (var reference in ExtractReferencedLinks(block))
+                referenceDefinitions.Add(reference);
+
+            yield return block;
+        }
+
+        document.Clear();
+
+        if (referenceDefinitions.Count <= 0) yield break;
+
+        var referenceBlock = new LinkReferenceDefinitionGroup();
+        foreach (var def in referenceDefinitions.OrderBy(d => d.Label))
+        {
+            def.Parent?.Remove(def);
+            referenceBlock.Add(def);
+        }
+
+        yield return referenceBlock;
+    }
+
+    private static IEnumerable<LinkReferenceDefinition> ExtractReferencedLinks(Block block) =>
+        block
+            .Descendants<LinkInline>()
+            .Select(li => li.Reference)
+            .Where(def => def is not null)!;
 }
--- ChangelogAutomation.Core/MarkdownToPlainTextConverter.cs
+++ ChangelogAutomation.Core/MarkdownToPlainTextConverter.cs
@@ -4,135 +4,142 @@ using Markdig.Renderers.Normalize;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 
-namespace ChangelogAutomation.Core
+namespace ChangelogAutomation.Core;
+
+public class MarkdownToPlainTextConverter : MarkdownConverterBase
 {
-    public class MarkdownToPlainTextConverter : MarkdownConverterBase
+    public async Task<PlainText> ExtractVersionSectionPlainText(MarkdownStream changelogMarkdown)
     {
-        public async Task<PlainText> ExtractVersionSectionPlainText(MarkdownStream changelogMarkdown)
+        var sectionDocument = await ExtractVersionDocument(changelogMarkdown);
+        return new PlainText(RenderToPlainText(sectionDocument));
+    }
+
+    private static string RenderToPlainText(MarkdownDocument document)
+    {
+        using var writer = new StringWriter();
+        writer.NewLine = "\n";
+        var renderer = new NormalizeRenderer(writer);
+
+        var firstBlock = true;
+        foreach (var block in document)
         {
-            var sectionDocument = await ExtractVersionDocument(changelogMarkdown);
-            return new PlainText(RenderToPlainText(sectionDocument));
+            if (!firstBlock) writer.WriteLine();
+            firstBlock = false;
+
+            AppendBlock(block, writer, renderer);
         }
 
-        private static string RenderToPlainText(MarkdownDocument document)
+        return writer.ToString();
+    }
+
+    private static void AppendBlock(Block block, StringWriter writer, NormalizeRenderer renderer)
+    {
+        switch (block)
         {
-            using var writer = new StringWriter { NewLine = "\n" };
-            var renderer = new NormalizeRenderer(writer);
+            case HeadingBlock heading:
+                writer.Write('[');
+                if (heading.Inline is { } hInlines)
+                {
+                    foreach (var item in hInlines)
+                        AppendInline(item, writer, renderer);
+                }
+                writer.WriteLine(']');
+                break;
 
-            var firstBlock = true;
-            foreach (var block in document)
-            {
-                if (!firstBlock) writer.WriteLine();
-                firstBlock = false;
+            case QuoteBlock quote:
+                foreach (var item in quote)
+                {
+                    using var nestedWriter = new StringWriter();
+                    nestedWriter.NewLine = "\n";
+                    var nestedRenderer = new NormalizeRenderer(nestedWriter);
+                    AppendBlock(item, nestedWriter, nestedRenderer);
 
-                AppendBlock(block, writer, renderer);
-            }
+                    var quotedLines = nestedWriter.ToString().Split("\n");
+                    for (var i = 0; i < quotedLines.Length; ++i)
+                    {
+                        var line = quotedLines[i];
+                        if (i == quotedLines.Length - 1 && line == "") continue;
+                        writer.WriteLine($"> {line}");
+                    }
+                }
+                break;
 
-            return writer.ToString();
+            case CodeBlock code:
+                for (var index = 0; index < code.Lines.Count; ++index)
+                    writer.WriteLine(code.Lines.Lines[index]);
+                break;
+
+            case ParagraphBlock paragraph:
+                if (paragraph.Inline is { } pInlines)
+                {
+                    foreach (var item in pInlines)
+                        AppendInline(item, writer, renderer);
+                }
+                writer.WriteLine();
+                break;
+
+            case ThematicBreakBlock:
+                writer.WriteLine("* * *");
+                break;
+
+            case ListBlock { BulletType: '1' } list:
+                var number = 1;
+                foreach (var item in list)
+                {
+                    writer.Write($"{number++}. ");
+                    AppendBlock(item, writer, renderer);
+                }
+                break;
+
+            case ListBlock list:
+                foreach (var item in list)
+                {
+                    writer.Write("- ");
+                    AppendBlock(item, writer, renderer);
+                }
+                break;
+
+            case LinkReferenceDefinitionGroup _:
+                // Handled automatically when processing links; no output is expected.
+                break;
+
+            case ContainerBlock container:
+                foreach (var item in container)
+                    AppendBlock(item, writer, renderer);
+                break;
+
+            default:
+                renderer.Render(block);
+                break;
         }
+    }
 
-        private static void AppendBlock(Block block, StringWriter writer, NormalizeRenderer renderer)
+    private static void AppendInline(Inline inline, StringWriter writer, NormalizeRenderer renderer)
+    {
+        switch (inline)
         {
-            switch (block)
-            {
-                case HeadingBlock heading:
-                    writer.Write('[');
-                    foreach (var item in heading.Inline)
-                        AppendInline(item, writer, renderer);
-                    writer.WriteLine(']');
-                    break;
+            case LineBreakInline:
+                writer.Write(' ');
+                break;
 
-                case QuoteBlock quote:
-                    foreach (var item in quote)
-                    {
-                        using var nestedWriter = new StringWriter {NewLine = "\n"};
-                        var nestedRenderer = new NormalizeRenderer(nestedWriter);
-                        AppendBlock(item, nestedWriter, nestedRenderer);
+            case EmphasisInline emphasis:
+                foreach (var item in emphasis)
+                    AppendInline(item, writer, renderer);
+                break;
 
-                        var quotedLines = nestedWriter.ToString().Split("\n");
-                        for (var i = 0; i < quotedLines.Length; ++i)
-                        {
-                            var line = quotedLines[i];
-                            if (i == quotedLines.Length - 1 && line == "") continue;
-                            writer.WriteLine($"> {line}");
-                        }
-                    }
-                    break;
+            case CodeInline code:
+                writer.Write(code.Content);
+                break;
 
-                case CodeBlock code:
-                    for (var index = 0; index < code.Lines.Count; ++index)
-                        writer.WriteLine(code.Lines.Lines[index]);
-                    break;
+            case LinkInline link:
+                foreach (var item in link)
+                    AppendInline(item, writer, renderer);
+                writer.Write($" ({link.Url})");
+                break;
 
-                case ParagraphBlock paragraph:
-                    foreach (var item in paragraph.Inline)
-                        AppendInline(item, writer, renderer);
-                    writer.WriteLine();
-                    break;
-
-                case ThematicBreakBlock:
-                    writer.WriteLine("* * *");
-                    break;
-
-                case ListBlock { BulletType: '1' } list:
-                    var number = 1;
-                    foreach (var item in list)
-                    {
-                        writer.Write($"{number++}. ");
-                        AppendBlock(item, writer, renderer);
-                    }
-                    break;
-
-                case ListBlock list:
-                    foreach (var item in list)
-                    {
-                        writer.Write("- ");
-                        AppendBlock(item, writer, renderer);
-                    }
-                    break;
-
-                case LinkReferenceDefinitionGroup _:
-                    // Handled automatically when processing links; no output is expected.
-                    break;
-
-                case ContainerBlock container:
-                    foreach (var item in container)
-                        AppendBlock(item, writer, renderer);
-                    break;
-
-                default:
-                    renderer.Render(block);
-                    break;
-            }
-        }
-
-        private static void AppendInline(Inline inline, StringWriter writer, NormalizeRenderer renderer)
-        {
-            switch (inline)
-            {
-                case LineBreakInline:
-                    writer.Write(' ');
-                    break;
-
-                case EmphasisInline emphasis:
-                    foreach (var item in emphasis)
-                        AppendInline(item, writer, renderer);
-                    break;
-
-                case CodeInline code:
-                    writer.Write(code.Content);
-                    break;
-
-                case LinkInline link:
-                    foreach (var item in link)
-                        AppendInline(item, writer, renderer);
-                    writer.Write($" ({link.Url})");
-                    break;
-
-                default:
-                    renderer.Render(inline);
-                    break;
-            }
+            default:
+                renderer.Render(inline);
+                break;
         }
     }
 }
