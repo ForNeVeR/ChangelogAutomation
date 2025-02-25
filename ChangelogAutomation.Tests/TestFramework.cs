f662ee48b6a388fb89ed12269f31c84bbd4d9055
Friedrich von Never <friedrich@fornever.me>
2025-02-25 23:10:08 +0100

    TestFramework: update to NUnit v4 assertions

--- ChangelogAutomation.Tests/TestFramework.cs
+++ ChangelogAutomation.Tests/TestFramework.cs
@@ -3,23 +3,22 @@ using System.Text;
 using ChangelogAutomation.Core;
 using NUnit.Framework;
 
-namespace ChangelogAutomation.Tests
+namespace ChangelogAutomation.Tests;
+
+public static class TestFramework
 {
-    public static class TestFramework
+    private static string NormalizeLineEndings(this string t) => t.Replace("\r\n", "\n");
+
+    public static MarkdownStream Markdown(string text) =>
+        new(new MemoryStream(Encoding.UTF8.GetBytes(text.NormalizeLineEndings())));
+
+    public static void AssertMarkdownEqual(string expected, MarkdownText actual)
     {
-        private static string NormalizeLineEndings(this string t) => t.Replace("\r\n", "\n");
+        Assert.That(expected.NormalizeLineEndings(), Is.EqualTo(actual.Content));
+    }
 
-        public static MarkdownStream Markdown(string text) =>
-            new(new MemoryStream(Encoding.UTF8.GetBytes(text.NormalizeLineEndings())));
-
-        public static void AssertMarkdownEqual(string expected, MarkdownText actual)
-        {
-            Assert.AreEqual(expected.NormalizeLineEndings(), actual.Content);
-        }
-
-        public static void AssertPlainTextEqual(string expected, PlainText actual)
-        {
-            Assert.AreEqual(expected.NormalizeLineEndings(), actual.Content);
-        }
+    public static void AssertPlainTextEqual(string expected, PlainText actual)
+    {
+        Assert.That(expected.NormalizeLineEndings(), Is.EqualTo(actual.Content));
     }
 }
