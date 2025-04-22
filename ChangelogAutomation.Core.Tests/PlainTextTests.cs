e1299ba8ce208f770e84eb383227587e887cb5c0
Никита Великий <mtyoungstz@outlook.com>
2025-04-23 02:27:34 +0500

    Replace verbatim strings with raw string literals

--- ChangelogAutomation.Core.Tests/PlainTextTests.cs
+++ ChangelogAutomation.Core.Tests/PlainTextTests.cs
@@ -2,163 +2,210 @@
 using NUnit.Framework;
 using static ChangelogAutomation.Core.Tests.TestFramework;
 
-namespace ChangelogAutomation.Core.Tests
+namespace ChangelogAutomation.Core.Tests;
+
+public sealed class PlainTextTests
 {
-    public class PlainTextTests
+    private readonly MarkdownToPlainTextConverter _converter = new();
+
+    private async Task ConversionTest(string input, string expectedOutput)
     {
-        private readonly MarkdownToPlainTextConverter _converter = new();
-
-        private async Task ConversionTest(string input, string expectedOutput)
-        {
-            using var source = Markdown(input);
-            var result = await _converter.ExtractVersionSectionPlainText(source);
-            AssertPlainTextEqual(expectedOutput, result);
-        }
-
-        [Test]
-        public Task ParagraphsTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-Paragraph 1
-
-Paragraph 2
-
-Paragraph
-text
-test", @"Paragraph 1
-
-Paragraph 2
-
-Paragraph text test
-");
-
-        [Test]
-        public Task HeadersTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-### Header
-Content
-
-Content
-
-### Header
-
-Content", @"[Header]
-
-Content
-
-Content
-
-[Header]
-
-Content
-");
-
-        [Test]
-        public Task FormattingTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-**Bold**
-test
-_italic_ __bold__
-
-_Italic_
-
----", @"Bold test italic bold
-
-Italic
-
-* * *
-");
-
-        [Test]
-        public Task LinksTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-[Link 1](https://example.com/1)
-
-[Link 2][link2]
-
-## Version 0.9
-[link2]: https://example.com/2", @"Link 1 (https://example.com/1)
-
-Link 2 (https://example.com/2)
-");
-
-        [Test]
-        public Task ListsTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-1. Item 1.
-   Item 1.1.
-1. Item 2
-
-- Item 1
-- Item 2", @"1. Item 1. Item 1.1.
-2. Item 2
-
-- Item 1
-- Item 2
-");
-
-        [Test]
-        public Task QuoteTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-> Quote
-> Quote 2
-
-> Quote **3**
->
-> Quote 4
-
-> 1. Quoted list item 1
-> 2. Quoted list item 2
->
-> > Sub-quote.
-> > Sub-quote cont.", @"> Quote Quote 2
-
-> Quote 3
-> Quote 4
-
-> 1. Quoted list item 1
-> 2. Quoted list item 2
-> > Sub-quote. Sub-quote cont.
-");
-
-        [Test]
-        public Task CodeTest() =>
-            ConversionTest(@"# Changelog
-## Version 1.0
-```
-Code 1
-Code 2
-```
-
-    Code 1
-    Code 2
-
-Paragraph with `code 3`.", @"Code 1
-Code 2
-
-Code 1
-Code 2
-
-Paragraph with code 3.
-");
-
-        [Test]
-        public Task LinkBlockTest() =>
-            ConversionTest(@"# Changelog
-
-## Version 1.0
-- **Test line**
-- Another [test][link] line
-
-[unrelated]: https://example.com/unrelated
-[link]: https://example.com/", @"- Test line
-- Another test (https://example.com/) line
-
-");
+        using var source = Markdown(input);
+        var result = await _converter.ExtractVersionSectionPlainText(source);
+        AssertPlainTextEqual(expectedOutput, result);
     }
+
+    [Test]
+    public Task ParagraphsTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            Paragraph 1
+
+            Paragraph 2
+
+            Paragraph
+            text
+            test
+            """,
+            """
+            Paragraph 1
+
+            Paragraph 2
+
+            Paragraph text test
+
+            """);
+
+    [Test]
+    public Task HeadersTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            ### Header
+            Content
+
+            Content
+
+            ### Header
+
+            Content
+            """,
+            """
+            [Header]
+
+            Content
+
+            Content
+
+            [Header]
+
+            Content
+
+            """);
+
+    [Test]
+    public Task FormattingTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            **Bold**
+            test
+            _italic_ __bold__
+
+            _Italic_
+
+            ---
+            """,
+            """
+            Bold test italic bold
+
+            Italic
+
+            * * *
+
+            """);
+
+    [Test]
+    public Task LinksTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            [Link 1](https://example.com/1)
+
+            [Link 2][link2]
+
+            ## Version 0.9
+            [link2]: https://example.com/2
+            """,
+            """
+            Link 1 (https://example.com/1)
+
+            Link 2 (https://example.com/2)
+
+            """);
+
+    [Test]
+    public Task ListsTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            1. Item 1.
+               Item 1.1.
+            1. Item 2
+
+            - Item 1
+            - Item 2
+            """,
+            """
+            1. Item 1. Item 1.1.
+            2. Item 2
+
+            - Item 1
+            - Item 2
+
+            """);
+
+    [Test]
+    public Task QuoteTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            > Quote
+            > Quote 2
+
+            > Quote **3**
+            >
+            > Quote 4
+
+            > 1. Quoted list item 1
+            > 2. Quoted list item 2
+            >
+            > > Sub-quote.
+            > > Sub-quote cont.
+            """,
+            """
+            > Quote Quote 2
+
+            > Quote 3
+            > Quote 4
+
+            > 1. Quoted list item 1
+            > 2. Quoted list item 2
+            > > Sub-quote. Sub-quote cont.
+
+            """);
+
+    [Test]
+    public Task CodeTest() =>
+        ConversionTest(
+            """
+            # Changelog
+            ## Version 1.0
+            ```
+            Code 1
+            Code 2
+            ```
+
+                Code 1
+                Code 2
+
+            Paragraph with `code 3`.
+            """,
+            """
+            Code 1
+            Code 2
+
+            Code 1
+            Code 2
+
+            Paragraph with code 3.
+
+            """);
+
+    [Test]
+    public Task LinkBlockTest() =>
+        ConversionTest(
+            """
+            # Changelog
+
+            ## Version 1.0
+            - **Test line**
+            - Another [test][link] line
+
+            [unrelated]: https://example.com/unrelated
+            [link]: https://example.com/
+            """,
+            """
+            - Test line
+            - Another test (https://example.com/) line
+
+
+            """);
 }
